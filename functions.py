a29ece0d9ece2bb15e23277a04a0532d4ccc7c53
daly <dalymami741@outlook.com>
2024-04-09 12:52:00 -0400

    updated

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
-from surprise import Dataset, Reader, KNNBasic, KNNWithMeans, KNNBaseline, NMF, SVD, SVDpp, CoClustering, BaselineOnly
+from surprise import Dataset, Reader, KNNBasic, KNNWithMeans, KNNBaseline, NMF, SVD, SVDpp, CoClustering, BaselineOnly, accuracy
 import matplotlib.pyplot as plt
+import copy
+
 
 # Add a guassian noise to a dataframe
 def add_gaussian_noise(df, noise):
@@ -10,52 +12,6 @@ def add_gaussian_noise(df, noise):
     noisy_df['rating'] = np.clip(noisy_df['rating'], 1, 5)  # Keep the ratings between 1 and 5
     noisy_df['rating'] = noisy_df['rating'].round().astype(int)
     return noisy_df
-
-
-# Returns a complete dataframe of predicted ratings based on a trained model and an incomplete dataframe
-def predict_all_ratings(model, df):
-    user_ids = df['user id'].unique()
-    movie_ids = df['movie id'].unique()
-    predictions = []
-
-    for user_id in user_ids:
-        for movie_id in movie_ids:
-            prediction = model.predict(user_id, movie_id).est
-            predictions.append({'user id': user_id, 'movie id': movie_id, 'rating': prediction})
-
-    predictions_df = pd.DataFrame(predictions)
-    return predictions_df
-
-
-
-# Trains model on a dataframe
-def train_model(model_name,df):
-    models_dict = {
-        "KNN": KNNBasic(verbose=False),
-        "KNN with Means": KNNWithMeans(verbose=False),
-        "KNN Baseline": KNNBaseline(verbose=False),
-        "NMF": NMF(verbose=False),
-        "SVD (ALS)": SVD(verbose=False),
-        "SVD++": SVDpp(verbose=False),
-        "Co-clustering": CoClustering(verbose=False),
-        "BaselineOnly": BaselineOnly(verbose=False)
-    }
-    # Check if the model exists in the dictionary
-    if model_name in models_dict:
-      # Get the model associated to the it's name
-      model = models_dict[model_name]
-      # Transform the data for the surprise library
-      reader = Reader(rating_scale=(1, 5))
-      data = Dataset.load_from_df(df[['user id', 'movie id', 'rating']], reader)
-      trainset = data.build_full_trainset() # We don't need a test set, we train on the whole dataset
-      # Fit the model on the train set
-      model.fit(trainset)
-      return model
-    # Return None if the model name doesn't exist in the dictionary
-    else:
-      print("Unknown model.")
-      return None
-
 
 
 # Returns the rmse between two dataframes
@@ -71,8 +27,6 @@ def calculate_rmse(df1, df2):
     rmse = np.sqrt(mse)
 
     return rmse
-
-
 
 
 
@@ -108,44 +62,28 @@ def convert_to_movielens_format(oracle):
     return movielens_df
 
 
-
-# Returns the rmse of each model on a specific configuration of a complete dataset
-def evaluate_model_on_complete_oracle(model, complete_oracle_df, density_percentage, noise):
-  density = int(len(complete_oracle_df) * density_percentage) # Get the exact number of ratings to take based on the percentage
-  trainset = complete_oracle_df.sample(n=density, random_state=42)
-  testset = complete_oracle_df.drop(trainset.index)
+# Returns the RMSE of a model on a specific settings to generate a dataframe
+def evaluate_model(model, df, training_percentage, noise, complete_oracle):
+  num_training_ratings = int(len(df) * training_percentage) # Get the exact number of ratings to take based on the percentage
+  trainset = df.sample(n=num_training_ratings, random_state=42)
+  testset = df.drop(trainset.index)
   noisy_trainset = add_gaussian_noise(trainset, noise)
-  noisy_trainset['rating'] = noisy_trainset['rating'].round().astype(int) # TO VERIFY
-  model = train_model(model,noisy_trainset)
-  predicted_df = predict_all_ratings(model, testset)
-  rmse = calculate_rmse(predicted_df, complete_oracle_df)
+  noisy_trainset['rating'] = noisy_trainset['rating'].round().astype(int)
+  num_models_to_train=5
+  training_percentage=0.9
+  epsilon=0.05
+  models_list = combined_bagging_train(model,noisy_trainset, num_models_to_train, training_percentage, epsilon)
+  predicted_df = predict_all_ratings_combined_baggings(models_list, testset)
+  rmse = calculate_rmse(predicted_df, df) if complete_oracle == 0 else calculate_rmse(predicted_df, testset)
   return rmse
 
-# Returns the rmse of each model on a specific configuration of an incomplete dataset
-def evaluate_model_on_uncomplete_oracle(model, df, density_percentage, noise):
-    density = int(len(df) * density_percentage)
-    trainset = df.sample(n=density, random_state=42)
-    testset = df.drop(trainset.index)
-    noisy_trainset = add_gaussian_noise(trainset, noise)
-    noisy_trainset['rating'] = noisy_trainset['rating'].round().astype(int) # TO VERIFY
-    model = train_model(model,noisy_trainset)
-    predicted_df = predict_all_ratings(model, testset)
-    rmse = calculate_rmse(predicted_df, testset)
-    return rmse
+
+
+# ---------------------- GRAPHS DISPLAY --------------------------- #
 
 
 
-def get_model_performance(model, oracle, density_percentage, noise, complete_oracle):
-  if complete_oracle == 0:
-    rmse = evaluate_model_on_uncomplete_oracle(model, oracle, density_percentage, noise)
-    return rmse
-
-  else:
-    rmse = evaluate_model_on_complete_oracle(model, oracle, density_percentage, noise)
-    return rmse
-
-
-def plot_results(results,noise_levels,densities,models,df):
+def plot_results(results,noise_levels,densities,models,df) :
 
   # Convert results into a pandas dataframe
   df_results = pd.DataFrame(results)
@@ -153,9 +91,9 @@ def plot_results(results,noise_levels,densities,models,df):
   # First graph: RMSE based on the noise
   plt.figure(figsize=(10, 6))
   for model_name in models:
-      for density_percentage in densities:
-          df_filtered = df_results[(df_results['model'] == model_name) & (df_results['num_ratings'] == int(len(df) * density_percentage))]
-          plt.plot(df_filtered['noise_level'], df_filtered['rmse'], marker='o', label=f"{model_name}, {int(len(df) * density_percentage)} ratings")
+      for training_percentage in densities:
+          df_filtered = df_results[(df_results['model'] == model_name) & (df_results['num_ratings'] == int(len(df) * training_percentage))]
+          plt.plot(df_filtered['noise_level'], df_filtered['rmse'], marker='o', label=f"{model_name}, {int(len(df) * training_percentage)} ratings")
 
   plt.xlabel('Noise Level')
   plt.ylabel('RMSE')
@@ -181,7 +119,96 @@ def plot_results(results,noise_levels,densities,models,df):
   plt.grid(True)
 
   # values to print on the X axis
-  x_ticks_labels = [f"{density_percentage*100}% ({int(len(df) * density_percentage)})" for density_percentage in densities]
-  plt.xticks([int(len(df) * density_percentage) for density_percentage in densities], x_ticks_labels)
+  x_ticks_labels = [f"{training_percentage*100}% ({int(len(df) * training_percentage)})" for training_percentage in densities]
+  plt.xticks([int(len(df) * training_percentage) for training_percentage in densities], x_ticks_labels)
 
   plt.show()
+
+
+
+
+  # ----------------- BAGGING TRAINING ------------------- #
+
+
+
+
+def combined_bagging_train(model_name, df, num_models, training_percentage, epsilon):
+    models_dict = {
+        "KNN": KNNBasic(verbose=False),
+        "KNN with Means": KNNWithMeans(verbose=False),
+        "KNN Baseline": KNNBaseline(verbose=False),
+        "NMF": NMF(verbose=False),
+        "SVD (ALS)": SVD(verbose=False),
+        "SVD++": SVDpp(verbose=False),
+        "Co-clustering": CoClustering(verbose=False),
+        "BaselineOnly": BaselineOnly(verbose=False)
+    }
+
+    num_training_ratings = int(len(df) * training_percentage)
+    if model_name in models_dict:
+        base_model = models_dict[model_name]
+        models = []
+        weights = []  # List to store weights for each instance
+
+        for _ in range(num_models):
+            # Train initial model
+            model = copy.deepcopy(base_model)
+            sample_df = df.sample(n=num_training_ratings)
+            reader = Reader(rating_scale=(1, 5))
+            data = Dataset.load_from_df(sample_df[['user id', 'movie id', 'rating']], reader)
+            trainset = data.build_full_trainset()
+            model.fit(trainset)
+            models.append(model)
+
+            # Weighted resampling for subsequent models
+            if len(models) > 1: # We need at least 2 models to do the bagging
+                updated_weights = []  # Updated weights for the next training sample
+                for _, row in df.iterrows():
+                    user_id = row['user id']
+                    movie_id = row['movie id']
+                    rating = row['rating']
+
+                    # Predict using all models except the last one
+                    median_prediction = combined_bagging_predict(models,user_id,movie_id)
+
+                    # Calculate error and update weights
+                    error = abs(median_prediction - rating)
+                    updated_weight = error if error <= epsilon else error + epsilon
+                    updated_weights.append(updated_weight)
+
+                # Normalize updated weights
+                updated_weights_sum = sum(updated_weights)
+                updated_weights = [w / updated_weights_sum for w in updated_weights]
+                weights = updated_weights
+
+                # Resample using updated weights
+                indices = np.random.choice(df.index, size=num_training_ratings, replace=True, p=weights)
+                df = df.loc[indices].reset_index(drop=True)
+
+        return models
+    else:
+        print("Modèle inconnu.")
+        return None
+
+
+# Define bagging_predict to use median of all model predictions
+def combined_bagging_predict(models, user_id, movie_id):
+    predictions = [model.predict(user_id, movie_id).est for model in models]
+    median_prediction = np.median(predictions)
+    return median_prediction
+
+
+
+# Returns a complete dataframe of predicted ratings based on a trained model and an incomplete dataframe
+def predict_all_ratings_combined_baggings(models_list, df):
+    user_ids = df['user id'].unique()
+    movie_ids = df['movie id'].unique()
+    predictions = []
+
+    for user_id in user_ids:
+        for movie_id in movie_ids:
+            prediction = combined_bagging_predict(models_list,user_id, movie_id)
+            predictions.append({'user id': user_id, 'movie id': movie_id, 'rating': prediction})
+
+    predictions_df = pd.DataFrame(predictions)
+    return predictions_df
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matrix_factorization import *
 from random_trees import *
 
 # Oracle settings (1,586,126 rating)
-num_users = 1000
+num_users = 500
 num_movies = 100
 
 
@@ -23,22 +23,22 @@ num_movies = 100
 #complete_oracle = 1  # 1 if we have got a complete matrix of ratings and 0 if not.
 
 # Method 3 : Generate a complete oracle with random trees
-# genres = ["Action", "Comédie", "Drame", "Science-Fiction", "Horreur"]
-#leaf_probability = 0.3
-#oracle = generate_ratings_matrix(num_users, num_movies, genres, leaf_probability)
-#movielens_format_oracle = convert_to_movielens_format(oracle) # rinted values
-#df = movielens_format_oracle
-#complete_oracle = 1  # 1 if we have got a complete matrix of ratings and 0 if not.
+genres = ["Action", "Comédie", "Drame", "Science-Fiction", "Horreur"]
+leaf_probability = 0.3
+oracle = generate_ratings_matrix(num_users, num_movies, genres, leaf_probability)
+movielens_format_oracle = convert_to_movielens_format(oracle) # rinted values
+df = movielens_format_oracle
+complete_oracle = 1  # 1 if we have got a complete matrix of ratings and 0 if not.
 
 # Method 4 : import an existing dataset with missing ratings
-df = pd.read_csv("movielens100k.csv")
-complete_oracle = 0  # 1 if we have got a complete matrix of ratings and 0 if not.
+#df = pd.read_csv("movielens100k.csv")
+#complete_oracle = 0  # 1 if we have got a complete matrix of ratings and 0 if not.
 
 
 # Set the testing parameters
-densities = [0.9,0.8,0.7]
-noise_levels = [0.0,0.5,1.0]
-models = ["SVD++", "KNN", "SVD (ALS)"]
+densities = [0.9,0.8]
+noise_levels = [0.0,1.0]
+models = ["SVD (ALS)"]
 
 
 
@@ -48,13 +48,13 @@ results = []
 # Loop on the models
 for model_name in models:
   print(f"Model : {model_name}")
-  for density_percentage in densities:
-    print (f"percentage of training ratings : {density_percentage*100}%")
+  for training_percentage in densities:
+    print (f"percentage of training ratings : {training_percentage*100}%")
     for noise_level in noise_levels:
       print(f"Noise Level : {noise_level}")
-      rmse = get_model_performance(model_name, df, density_percentage, noise_level, complete_oracle)
-      num_training_ratings = int(len(df) * density_percentage)
-      print (f"RMSE of {model_name} on the oracle of noise {noise_level} , and {density_percentage*100}% = ({num_training_ratings}) ratings is : {rmse:.4f}")
+      rmse = evaluate_model(model_name, df, training_percentage, noise_level, complete_oracle)
+      num_training_ratings = int(len(df) * training_percentage)
+      print (f"RMSE of {model_name} on the oracle of noise {noise_level} , and {training_percentage*100}% = ({num_training_ratings}) ratings is : {rmse:.4f}")
       # Store the results
       results.append({
         'model': model_name,
@@ -62,5 +62,5 @@ for model_name in models:
         'noise_level': noise_level,
         'rmse': rmse
         })
-# Plot the models performances
+
 plot_results(results,noise_levels,densities,models,df)
